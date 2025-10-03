dd2c4d08ba04c2a5553e75fa06687e319e25f964
Peter <Petaarz@hotmail.com>
2025-10-03 13:09:47 +0200

    ROS2 package for executing MiR missions

---
+++ sdl_packages/mir/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'mir'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ppj',
+    maintainer_email='ppje21@hotmail.com',
+    description='Package for controlling the mobile MiR200 robot based in ROS2',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mir_server_node = mir.mir_server_node:main',
+            'mir_position_node = mir.mir_check_position_node:main',
+            'mir_turn_90 = mir.mir_turn_90:main',
+        ],
+    },
+)
